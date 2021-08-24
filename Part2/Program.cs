8fe62340d2579fe7f406adba1f06259900d2fc63
spostew <spencerunitt0@gmail.com>
2021-08-23 20:01:31 -0700

    Part2 added

---
+++ Part2/Program.cs
@@ -0,0 +1,38 @@
+﻿/// Chapter No. 1		Exercise No. 2
+/// File Name: MyFirstProgram.java
+/// @author: Spencer Unitt
+/// Date:  August 23, 2021
+///
+/// Problem Statement: Create a picture with characters and print the picture out to the console screen.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Create a handful of lines to write to the console
+/// 2) Make a picture with characters using each line
+/// 3) Display the result to the user
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(@"  /\~-~/\");
+            Console.WriteLine(@" |(@)(@) |");
+            Console.WriteLine(@" \ ~ V ~ /");
+            Console.WriteLine(@" / ****  \");
+            Console.WriteLine(@" | ***** |");
+            Console.WriteLine(@"  \**** /");
+            Console.WriteLine(@" --/\-/\-");
+            Console.ReadLine();
+        }
+    }
+}
